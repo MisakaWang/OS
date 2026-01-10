b945bfc599455b800d724a3325df8d8021920ac8
MisakaWang <wss614562130@gmail.com>
2026-01-09 23:59:47 -0800

    页目录项和页

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ dd if=OS_CODE/boot/mbr.bin of=hd64M.img bs=512 count=1 conv=notrunc
 
 # 写入 Loader 到第 2 扇区 (注意 seek=2)
 # count=4 是为了确保即使 Loader 变大也能完整写入
-dd if=OS_CODE/boot/loader.bin of=hd64M.img bs=512 count=2 seek=2 conv=notrunc
+dd if=OS_CODE/boot/loader.bin of=hd64M.img bs=512 count=4 seek=2 conv=notrunc
 
-bochs -f bochsrc.disk -q
+bochs -f bochsrc.disk
